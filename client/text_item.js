5a2c57e13419407129872e41e6db58b170c12ff9
LB (Ben) Johnston <github@lb.ee>
2015-05-25 17:23:26 +1000

    get context triples showing nicer

--- client/text_item.js
+++ client/text_item.js
@@ -2,7 +2,13 @@
 
 Template.textItem.helpers({
   url: function () {
-    return this.url || 'use flowrouter here';
+    return this.url || FlowRouter.path('page', {id: this.id});
+  },
+  class: function () {
+    if (this.class) {
+      return this.class;
+    }
+    return 'text item';
   },
   name: function () {
     var page = lyrid.pages.findOne({_id: this.id});

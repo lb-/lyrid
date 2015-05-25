6d434ab1e0deaeaa2189434d2d174cbfb0473adf
LB (Ben) Johnston <github@lb.ee>
2015-05-25 17:37:06 +1000

    Triples representation mark 2
    This time showing too many triples (all variations)

--- client/context.js
+++ client/context.js
@@ -33,19 +33,41 @@ Template.context.helpers({
     //group by subject first
     var groupedByContext = _.groupBy(triples, 'context');
     var result = [];
-    var types = ['subject', 'predicate', 'object'];
+    var types = {
+      subject: { groupBy: 'predicate' },
+      predicate: { groupBy: 'object' },
+      object: { groupBy: 'predicate' },
+    };
     //not super sure on next steps
+    // _.each(groupedByContext, function (itemsForContext, context) {
+    //   _.each(_.groupBy(itemsForContext, 'predicate'), function (items, predicate) {
+    //     // the relevantId will NOT be the predicate
+    //     // the relvantId will NOT be the pageId
+    //     items = _.map(items, function (item) {
+    //       var ids = [item.subject, item.predicate, item.object];
+    //       item.relevantId = _.difference(ids, [predicate, pageId])[0];
+    //       return item;
+    //     });
+    //     console.log(items);
+    //     result.push({title: predicate, items: items});
+    //   });
+    // });
     _.each(groupedByContext, function (itemsForContext, context) {
-      _.each(_.groupBy(itemsForContext, 'predicate'), function (items, predicate) {
-        // the relevantId will NOT be the predicate
-        // the relvantId will NOT be the pageId
-        items = _.map(items, function (item) {
-          var ids = [item.subject, item.predicate, item.object];
-          item.relevantId = _.difference(ids, [predicate, pageId])[0];
-          return item;
+      //for each context; subject, predicate, object
+      _.each(types, function (typeMapping, typeName) {
+
+        //if the current page is the subject, group by predicate & list the object
+        //if the current page is the predicate, group by object & list the subject
+        //if the current page is object, group by predicate and list the subject
+        _.each(_.groupBy(itemsForContext, typeMapping.groupBy), function (items, groupingContextItem) {
+          items = _.map(items, function (item) {
+            var ids = [item.subject, item.predicate, item.object];
+            item.relevantId = _.difference(ids, [groupingContextItem, pageId])[0];
+            return item;
+          });
+          // console.log(items);
+          result.push({title: groupingContextItem, items: items});
         });
-        console.log(items);
-        result.push({title: predicate, items: items});
       });
     });
     return result;
